56c995d9ab11fbf0823cb6c831463a0f403973cc
Damien Epiard <damien.epiard@gmail.com>
2020-04-03 22:17:15 +0200

    First add basic case.

--- calculator.js
+++ calculator.js
@@ -7,6 +7,26 @@ function operate(operator, a, b) {
   return operator (a,b);
 }
 
+function operateAll () {
+    console.log('operateAll: ' + op1 + ' ' + op2 );
+    outputBuffer = '' + operate (add, op1, op2);
+}
+
+//function createOperatorKeys()
+function addListeners()
+{
+    /*let keyboard = document.getElementById('keyboard');
+    let key = document.createElement('button');
+    key.dataset.add
+    key.classList.add('key');
+    key.classList.add('operator-key');
+    key.style.gridArea = 'eq';*/
+    let operatorKeys = document.getElementsByClassName('key');
+    for (key of operatorKeys) {
+        key.addEventListener('click', keyPressed);
+    }
+}
+
 function createNumKeys()
 {
     let keyboard = document.getElementById('keyboard');
@@ -14,21 +34,92 @@ function createNumKeys()
     {
         let key = document.createElement('button');
         key.classList.add('key');
-        key.style.gridArea = 'n'+i;
-        key.addEventListener('click', keyPressed);      
+        key.style.gridArea = 'n'+ i;
+        key.dataset.value = i;
+        //key.addEventListener('click', keyPressed);      
         key.textContent = i;
         keyboard.appendChild(key);
     }   
 }
 
 function keyPressed(e) {
-    
+
+    console.log('value: ' + this.dataset.value);
+    const value = this.dataset.value;
+
+    if (value == 'reset') resetAll();  
+
+    else if (value == 'add') {
+        currentOperator = value;
+        // need to have both operands valid to compute
+        if (op1 == undefined) { 
+            op1 = flushInput();
+        }
+        else { // both operands valid
+            op2 = flushInput();
+            //operateAll();
+            // op1 = op2;
+            // op2 = undefined;
+        }   
+    }
+    else if(value =='equal')
+    {   
+        op2 = flushInput();
+        operateAll();
+        resetOperands();
+    }
+    else if (!isNaN(value)) { // num keys
+        let number = value;
+        inputBuffer += number;
+        outputBuffer = inputBuffer; 
+    }
+    else {
+    }
+    updateScreen();
+}
+
+function flushInput() {
+    let temp = parseInt(inputBuffer);
+    console.log({temp});
+    inputBuffer = '';
+    return temp;
+}
+
+function resetOperands() {
+    op1 = undefined;
+    op2 = undefined;
+}
+
+function resetAll() {
+    inputBuffer = '';
+    outputBuffer = '';
+    op1 = undefined;
+    op2 = undefined;
+    currentOperator = '';
+}
+
+let inputBuffer;
+let outputBuffer;
+let op1, op2;
+let currentOperator;
+
+function updateScreen() {
+  let screenOutput = document.getElementById('screen-output');
+  screenOutput.textContent = outputBuffer.length ? outputBuffer : '0';  
 }
 
 createNumKeys();
-console.log (operate (add, 3, -4));
+addListeners();
+resetAll();
+//createOperatorKeys();
+updateScreen();
 
-module.exports = {
-	add,
-	subtract
-}
+// let a = '';
+// a = ''+3;
+// console.log(typeof a);
+//console.log (operate (add, 3, -4));
+
+// module.exports = {
+// 	add,
+// 	subtract
+// }
