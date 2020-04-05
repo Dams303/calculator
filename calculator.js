ac71194fd6d07648e9a0fda8e08334f7c5a0802d
Damien Epiard <damien.epiard@gmail.com>
2020-04-06 00:11:33 +0200

    Basic unit tests working.

--- calculator.js
+++ calculator.js
@@ -1,34 +1,49 @@
 
 //////////////////////////////////////////////////////////
-const operators = ['*', '/', '+', '-'];
+//const operators = ['*', '/', '+', '-'];
+const operators = ['mul', 'div', 'add', 'sub'];
 
 let isOperator = (op) => operators.includes(op);
 
 function createCalculator() {
     let c = {};
     
-    c.operatorsFunc = [ {op: '*', func: c.multiply},
-                        {op: '/', func: c.divide},
-                        {op: '+', func: c.add}, 
-                        {op: '-', func: c.substract}];
+    c.add = (a, b) => a + b;
+    c.substract = (a, b) => a - b;
+    c.multiply = (a, b) => a * b;
+    c.divide = (a, b) => a / b;
+    c.operate = (operator, a, b) => operator(a, b);
+
+    c.operatorsFunc = [ {op: 'mul', func: c.multiply},
+                        {op: 'div', func: c.divide},
+                        {op: 'add', func: c.add}, 
+                        {op: 'sub', func: c.substract}];
     c.input = ['0'];
     c.currentPos = () => c.input.length -1; 
-    // interface methods
     c.currentInput = () => c.input[c.currentPos()]; 
     c.isCurrentOperator = () => isOperator (c.currentInput());
+
+    // interface methods
     c.addDigit = (digit) => {
         if (isCurrentOperator())
             c.input[c.currentPos()+1] = digit; //go to next digit   /*c.input = input.replace(/^0+/g, '');*/
         else { 
             if (c.currentPos() == 0 && c.currentInput() == '0' && digit == '0') {} // do nothing
             else c.input[c.currentPos()] += digit; // happen digits        
-        }           
+        }
+        //callDisplayListener();
     }
     c.addNumber = (number) => { (c.currentPos()==0) ? c.input[0] = number.toString() :
-                                                     c.input.push(number.toString()) };
-    c.addPoint = () => { c.input[c.currentPos()] += !c.input[c.currentPos()].includes('.') ? '.' : '' };
+                                                     c.input.push(number.toString()) };  // for testing purpose
+
+    c.addPoint = () => {
+         c.input[c.currentPos()] += !c.input[c.currentPos()].includes('.') ? '.' : '';
+         //callDisplayListener(); 
+    }
+
     c.addOperator = (op) => {        
         isOperator(c.currentInput()) ? c.input[c.currentPos()] = op : c.input[c.currentPos()+1] = op;
+        //callDisplayListener();
     }
     
     c.undo = () => {
@@ -41,38 +56,34 @@ function createCalculator() {
     c.reset = () => c.input = [];
     c.getDisplay = () => disp = c.input.reduce((out, cell) => out+=cell, '');
 
+
     c.processWithOperator = (op) => {
-        const operator = c.operatorsFunc.find((elt) => elt.op == op); // lookup for operators
+        let operator = c.operatorsFunc.find((elt) => elt.op == op); // lookup for operators
+
         while ( (index = c.input.indexOf(op)) != -1) {
             const op1 = parseFloat(c.input[index-1]);
             const op2 = parseFloat(c.input[index+1]);
             c.input.splice(index-1, 3, c.operate(operator.func, op1, op2));
         }
-    };
-
-    c.equal = () => operators.forEach(c.processWithOperator);
-
-//            if(c.callDisplayListener)
-//              c.callDisplayListener(c.operands[c.operands.length-1]);
-        
-    c.add = (a, b) => a + b;
-    c.substract = (a, b) => a - b;
-    c.multiply = (a, b) => a * b;
-    c.divide = (a, b) => a / b;
-    c.operate = (operator, a, b) => operator(a, b);
-    c.registerDisplayListener = (callback) => c.callDisplayListener = callback;
+    }
+    c.equal = () => {
+        operators.forEach(c.processWithOperator);
+        //callDisplayListener();
+        return c.input[0];
+    }
+    
+    //c.callDisplayListener = () => c.displayListener(c.getDisplay());
+    //c.addDisplayListener = (listener) => c.displayListener = listener;
 
     return c;
 }
 //////////////////////////////////////////////////////////
 // test new interface:
-let c = createCalculator();
-c.addNumber(1); 
-c.addOperator('+'); 
-c.addNumber(3);
-console.log('test1:' + c.equal());
-// c.addNumber(1); c.addOperator(c.multiply); c.addNumber(3); console.log('test2:' + c.equal());
-
+{let c = createCalculator(); 
+c.addNumber(1); c.addOperator('add'); c.addNumber(3); console.log('test1+4=5:' + c.equal());}
+{let c = createCalculator();
+ c.addNumber(4); c.addOperator('add'); c.addNumber(2); c.addOperator('mul'); c.addNumber(3); console.log('test4+2*3=10:' + c.equal());}
+//////////////////////////////////////////////////////////
 
 function addListeners() {
     let keys = document.getElementsByClassName('key');
@@ -85,32 +96,42 @@ function createNumKeys() {
     let keyboard = document.getElementById('keyboard');
     for (let i = 0; i < 10; ++i) {
         let key = document.createElement('button');
+        key.dataset.num = i;
         key.classList.add('key');
         key.style.gridArea = 'n' + i;
-        key.dataset.value = i;
         key.textContent = i;
         keyboard.appendChild(key);
     }
 }
 
 function keyPressed(e) {
-    console.log('value: ' + this.dataset.value);
-    const value = this.dataset.value;
+    console.log('value: ' + this.dataset);
+    //const value = this.dataset.value;
+    if ('num' in this.dataset)
+        c.addDigit(this.dataset.num);
+    else if ('op' in this.dataset)
+        c.addOperator(this.dataset.op);
+    else if ('action' in this.dataset)
+       c[this.dataset.action]  // undo, equal, reset
+    else if ('sep' in this.dataset)
+        c.addPoint();      
+    else 
+        console.log('unknown action:');
+    
+    updateDisplay();
 }
-updateScreen();
 
-function updateScreen() {
-    //let mainOutput = document.getElementById('main-output');
-    //mainOutput.textContent = outputBuffer.length ? outputBuffer : '0';
+let mainOutput = document.getElementById('main-output');
+let secondaryOutput = document.getElementById('secondary-output');
 
-    //let secondaryOutput = document.getElementById('secondary-output');
-    //secondaryOutput.textContent = 'Ans';
+function updateDisplay() {    
+    mainOutput.textContent = c.getDisplay();
+    secondaryOutput.textContent = 'Ans';
 }
-
+let c = createCalculator();
 createNumKeys();
-//addListeners();
-//resetAll();
-//updateScreen();
+addListeners();
+updateDisplay();
 
 // module.exports = {
 // // 	add, substract
