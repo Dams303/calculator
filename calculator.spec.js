9ec93993dc7b655cc28a622e0d48c999bef4015f
Damien Epiard <damien.epiard@gmail.com>
2020-04-07 18:01:50 +0200

    Added unit tests.

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,19 +1,11 @@
-const calculator = require('./calculator.js')
-
-
-//////////////////////////////////////////////////////////
-// Unit tests Calculator
-/*
-{
-    let c = createCalculator();
-    c.addNumber(2); c.addOperator('div'); c.addNumber(3); console.log('test max decimals: 2/3 = :' + c.equal());
-}
-{
-    let c = createCalculator();
-    c.addNumber(56); c.addOperator('mul'); c.addNumber(9.32); console.log('test: 56x9.32 = :' + c.equal());
-}*/
+require('./calculator.js')
 
 describe('add', function() {
+	it('basic add', function() {
+		let c = createCalculator();
+		c.addNumber(13); c.addOperator('add'); c.addNumber(34);
+		expect(c.equal()).toEqual([ '47' ]);
+	});
     it('incomplete add', function() {
 		let c = createCalculator();
 		c.addNumber(1); c.addOperator('add'); 
@@ -28,5 +20,10 @@ describe('add', function() {
 		let c = createCalculator();
 		c.addNumber(2); c.addOperator('div'); c.addNumber(3); 
 		expect(c.equal()).toEqual([ '0.666666667' ]);
+	});
+	it('test decimal zeros triming', function() {
+		let c = createCalculator();
+		c.addNumber(56); c.addOperator('mul'); c.addNumber(9.32);
+		expect(c.equal()).toEqual([ '521.92' ]);
 	});
 });
