20b75e42cc80dd9e9fb48bf27cd93309f75938d1
Damien Epiard <damien.epiard@gmail.com>
2020-04-07 13:28:39 +0200

    Fixed point bug, added max decimal, remove trailing zeros.

--- calculator.js
+++ calculator.js
@@ -7,10 +7,20 @@ function createCalculator() {
     c.divide = (a, b) => a / b;
     c.operate = (operator, a, b) => operator(a, b);
 
-    c.operatorsFunc = [{ op: 'mul', out: '×', func: c.multiply },
-    { op: 'div', out: '÷', func: c.divide },
-    { op: 'add', out: '+', func: c.add },
-    { op: 'sub', out: '-', func: c.substract }];
+    c.operatorsFunc = [
+      { op: 'mul', out: ' × ', func: c.multiply,    operands: 2 },
+      { op: 'div', out: ' ÷ ', func: c.divide,      operands: 2 }, 
+      { op: 'add', out: ' + ', func: c.add,         operands: 2 },
+      { op: 'sub', out: ' - ', func: c.substract,   operands: 2 }];
+
+    c.validate = () => {
+        let checkOperandsValid = (cell, index) => {
+            if (c.isOperator(cell) /*&& cell.operands == 2*/)  // only check for operators with 2 operands
+                return !isNaN(c.input[index-1]) && !isNaN(c.input[index+1]);
+            return true;
+        }
+        return c.input.every(checkOperandsValid);
+    }
 
     c.operators = c.operatorsFunc.map(elt => elt.op);
     c.isOperator = op => c.operators.includes(op);
@@ -24,7 +34,7 @@ function createCalculator() {
     // interface methods
     c.addDigit = (digit) => {
         if (c.isCurrentOperator())
-            c.input[c.currentPos() + 1] = digit; //go to next digit   /*c.input = input.replace(/^0+/g, '');*/
+            c.input[c.currentPos() + 1] = digit;
         else {
             if (c.currentPos() == 0 && c.currentInput() == '0') {
                 if (digit == '0');
@@ -38,8 +48,11 @@ function createCalculator() {
         c.input.push(number.toString())
     };  // for unit testing purpose
 
-    c.addPoint = () => {
-        c.input[c.currentPos()] += !c.input[c.currentPos()].includes('.') ? '.' : '';
+    c.addPoint = () => { 
+        if (c.isOperator(c.currentInput())) 
+            c.input[c.currentPos()+1] = '0.';
+        else 
+            c.input[c.currentPos()] += !c.input[c.currentPos()].includes('.') ? '.' : '';
     }
 
     c.addOperator = (op) => {
@@ -59,21 +72,26 @@ function createCalculator() {
     c.getDisplay = () => c.generateDisplay(c.input);
 
     c.findOperatorByOp = (op) => c.operatorsFunc.find((elt) => elt.op == op);
+
+    const MAX_DECIMALS = 9;
     c.processWithOperator = (op) => {
         let operator = c.findOperatorByOp(op); // lookup for operators
 
         while ((index = c.input.indexOf(op)) != -1) {
             const op1 = parseFloat(c.input[index - 1]);
             const op2 = parseFloat(c.input[index + 1]);
-            c.input.splice(index - 1, 3, c.operate(operator.func, op1, op2).toString());
-        }
+            c.input.splice(index - 1, 3, c.operate(operator.func, op1, op2));
     }
     c.equal = () => {
-        c.feedHistory();
-        console.table(c.history);
-        c.operators.forEach(c.processWithOperator);
-        //console.table(c.input);
-        return c.input[0];
+        if (c.validate()) {
+            c.feedHistory();
+            //console.table(c.history);
+            c.operators.forEach(c.processWithOperator);
+            c.input[0] = parseFloat( c.input[0].toFixed(MAX_DECIMALS)).toString();
+
+            console.table(c.input);
+        }
+        return c.input;
     }
     const HISTORY_SIZE = 10;
     c.feedHistory = () => {
@@ -93,18 +111,28 @@ function createCalculator() {
 {
  //   let c = createCalculator();
  //   c.addNumber(1); c.addOperator('add'); c.addNumber(3); console.log('test1+3=5:' + c.equal());
+    let c = createCalculator();
+    c.addNumber(1); c.addOperator('add'); console.log('test: 1,add == ' + c.equal());
 }
 {
-//    let c = createCalculator();
-//    c.addNumber(4); c.addOperator('add'); c.addNumber(2); c.addOperator('mul'); c.addNumber(3); console.log('test4+2*3=10:' + c.equal());
+    let c = createCalculator();
+    c.addNumber(4); c.addOperator('add'); c.addNumber(2); c.addOperator('mul'); c.addNumber(3); console.log('test4+2*3=10:' + c.equal());
+}
+{
+    let c = createCalculator();
+    c.addNumber(2); c.addOperator('div'); c.addNumber(3); console.log('test max decimals: 2/3 = :' + c.equal());
+}
+{
+    let c = createCalculator();
+    c.addNumber(56); c.addOperator('mul'); c.addNumber(9.32); console.log('test: 56x9.32 = :' + c.equal());
 }
 //////////////////////////////////////////////////////////
-// ** Known bugs: 
-// seq :5 + 7 = 5 doesnt reinit the seq to 5 -> look for a solution.
-// 
-// ** Improvments: 
-// ans and history feature: 70% done.
-// trim zero and rounding decimals.
+// ** Known bugs:
+
+// ** Improvments:
+// ans and history feature: 70% done: When to display it : 
+// after equal if digit then erase the previous cell: 
+// allow minus sign after other operators.
 
 //////////////////////////////////////////////////////////
 
@@ -165,11 +193,11 @@ function handleKeyDown(key) {
 }
 
 document.onkeydown = event => { 
-    if(!event.key.startsWith('F')) // allow F12 shorcuts for Chrome dev environment.
+    if(!event.key.startsWith('F')) // allow Fn shorcuts for Chrome dev environment.
         event.preventDefault();  
     handleKeyDown(event.key);
 }
 
-// module.exports = {
-// // 	add, substract
+//  module.exports = {
+//    createCalculator
 // }
--- calculator.spec.js
+++ calculator.spec.js
@@ -2,8 +2,9 @@ const calculator = require('./calculator.js')
 
 
 describe('add', function() {
-    it('adds 0 and 0', function() {
-		expect(calculator.add(0,0)).toEqual(0);
+    it('incomplete add', function() {
+		let c = createCalculator(); c.addNumber(1); c.addOperator('add'); 
+		expect(c.equal()).toEqual('1+add');
 	});
     xit('substrats 0 and 0', function() {
 		expect(calculator.subtract(0,0)).toEqual(0);
